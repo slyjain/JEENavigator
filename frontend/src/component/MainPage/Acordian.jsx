097e3a7123a4af0ec7c513289adcb7fbd1b4bb94
slyjain <jain.samyak.3210@gmail.com>
2024-08-04 10:48:18 +0000

    Add ProgressBar

--- frontend/src/component/MainPage/Acordian.jsx
+++ frontend/src/component/MainPage/Acordian.jsx
@@ -1,38 +1,45 @@
-import React from 'react'
-import data from './data'
-import { Box } from '@chakra-ui/react'
+import React from 'react';
+import data from './data';
+import { Box } from '@chakra-ui/react';
+import YouTube from "react-youtube";
+import ProgressBar from "@ramonak/react-progress-bar";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
-    
-  } from '@chakra-ui/react'
-const Acordian = () => {
-  return (
-    <div><Accordion allowToggle>
-        
-    
-    {data.map((item,index)=>{
-        return(
-        <div> <AccordionItem>
-      <h2>
-        <AccordionButton>
-          <Box as='span' flex='1' textAlign='left'>
-            {item.day}
-          </Box>
-          <AccordionIcon />
-        </AccordionButton>
-      </h2>
-      <AccordionPanel pb={4}>
-        {item.topicName}
-      </AccordionPanel>
-    </AccordionItem></div>)})
-  }
-    
-  </Accordion></div>
-  )
-}
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
 
-export default Acordian
+const AccordionComponent = ({setProgress}) => {
+  const _onReady = (event) => {
+    event.target.pauseVideo();
+  };
+  
+  return (
+    <Accordion allowToggle>
+      {data.map((item, index) => (
+        <AccordionItem key={index}>
+          <h2>
+            <AccordionButton>
+              <Box flex='1' textAlign='left'>
+              <input type="checkbox" onClick={()=>{
+                setProgress((e)=>e+10)
+              }} />  {item.day}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <Box>
+              {item.topicName}
+              <YouTube videoId={item.videoId} opts={{ height: '390', width: '640' }} onReady={_onReady} />
+            </Box>
+          </AccordionPanel>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
+
+export default AccordionComponent;
--- frontend/src/component/MainPage/data.js
+++ frontend/src/component/MainPage/data.js
@@ -1,44 +1,54 @@
-const data=[
+const data = [
     {
-        day:"Day 1",
-        topicName:"Introduction to Straight Lines"
+        day: "Day 1",
+        topicName: "Introduction to Straight Lines",
+        videoId: "bfLhQU9SNZE" // Replace with actual video ID
     },
     {
-        day:"Day 2",
-        topicName:"Slope of a Line"
+        day: "Day 2",
+        topicName: "Slope of a Line",
+        videoId: "iUN37avSPcA" // Replace with actual video ID
     },
     {
-        day:"Day 3",
-        topicName:"Equation of a Line"
+        day: "Day 3",
+        topicName: "Equation of a Line",
+        videoId: "QWqfmSW887Y" // Replace with actual video ID
     },
     {
-        day:"Day 4",
-        topicName:"Intercept Form of a Line"
+        day: "Day 4",
+        topicName: "Intercept Form of a Line",
+        videoId: "2xQqg40kReM" // Replace with actual video ID
     },
     {
-        day:"Day 5",
-        topicName:"Point Slope Form of a Line"
+        day: "Day 5",
+        topicName: "Point Slope Form of a Line",
+        videoId: "someVideoId5" // Replace with actual video ID
     },
     {
-        day:"Day 6",
-        topicName:"Two Point Form of a Line"
+        day: "Day 6",
+        topicName: "Two Point Form of a Line",
+        videoId: "someVideoId6" // Replace with actual video ID
     },
     {
-        day:"Day 7",
-        topicName:"Slope Intercept Form of a Line"
+        day: "Day 7",
+        topicName: "Slope Intercept Form of a Line",
+        videoId: "someVideoId7" // Replace with actual video ID
     },
     {
-        day:"Day 8",
-        topicName:"Normal Form of a Line"
+        day: "Day 8",
+        topicName: "Normal Form of a Line",
+        videoId: "someVideoId8" // Replace with actual video ID
     },
     {
-        day:"Day 9",
-        topicName:"Distance of a Point from a Line"
+        day: "Day 9",
+        topicName: "Distance of a Point from a Line",
+        videoId: "someVideoId9" // Replace with actual video ID
     },
     {
-        day:"Day 10",
-        topicName:"Angle between two Lines"
+        day: "Day 10",
+        topicName: "Angle between two Lines",
+        videoId: "someVideoId10" // Replace with actual video ID
     }
-]
+];
 
-export default data
+export default data;
