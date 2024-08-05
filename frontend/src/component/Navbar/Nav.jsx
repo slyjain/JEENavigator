67dbf87f291ec0efaf06fa2b047a393327cf5c41
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-08-05 19:34:39 +0530

    dark mode by omkar

--- frontend/src/component/Navbar/Nav.jsx
+++ frontend/src/component/Navbar/Nav.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Button, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 const Nav = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Box bg="gray.800" p={4}>
-      <Flex justify="center" align="center">
+      <Flex justify="center" align="center" gap={4}>
         <Text color="white" fontSize="xl" fontWeight="bold">
           JEE-NAVIGATOR
         </Text>
+        <Button variant='outline' onClick={toggleColorMode} colorScheme='teal'>
+          {colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+        </Button>
       </Flex>
     </Box>
   );
