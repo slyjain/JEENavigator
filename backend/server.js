459f7be3dfa6a2d351e264989ebfd36be7adbf8f
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-08-08 19:09:14 +0530

    apireqest test

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express=require("express");
 const app=express();
-const dotenv=require("dotenv")
+const cors = require('cors');
+const dotenv=require("dotenv");
 dotenv.config();
+app.use(cors()); 
 const PORT=process.env.PORT;
-app.get("/",(req,res)=>{
-    res.status(200).send("JEENavigator is ONNNNN")
+
+
+app.get("/api/questions",(req,res)=>{
+
+    res.send("JEENavigator is ONNNNN");
+    console.log("apirequest");
 })
+
 app.listen(PORT,()=>{
     console.log(`listening on ${PORT}`)
     
