e2165e6188409fde984df6d45e143263ec766b36
slyjain <jain.samyak.3210@gmail.com>
2024-08-08 13:41:09 +0000

    Get req to frontend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const express=require("express");
 const app=express();
 const dotenv=require("dotenv")
 dotenv.config();
+
 const PORT=process.env.PORT;
-app.get("/",(req,res)=>{
-    res.status(200).send("JEENavigator is ONNNNN")
+app.get("/get",(req,res)=>{
+    res.status(200).json({
+        message:"JEE NAVIGATOR is connected"
+    })
 })
 app.listen(PORT,()=>{
     console.log(`listening on ${PORT}`)
